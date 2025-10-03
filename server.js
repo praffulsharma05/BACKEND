4aafb5bca6042772d15d10edc6a05e401cf096d3
praffulsharma05 <praffulsharma38@gmail.com>
2025-10-03 13:52:54 +0530

    commit

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ app.get("/login", (req, res) => {
   res.send("<h1>please login at chai aur code</h1>");
 });
 
+const githubData = {
+  login: "praffulsharma38@gmail.com",
+};
+
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port `);
+});
+app.get("/github", (req, res) => {
+  res.json(githubData);
 });
